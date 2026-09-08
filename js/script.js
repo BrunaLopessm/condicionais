e2a8e6ce8272f61af165c7b549f3e75040d83652
BRUNA LOPES SILVA MENDES <bruna.lsmendes@senacsp.edu.br>
2026-09-08 20:33:01 -0300

    condicionais

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let nota, resultado;
+
+function verificar(){
+    nota = Number(document.getElementById("nota").value);
+    resultado = document.getElementById("resultado");
+
+    if( nota < 5){
+        resultado.innerHTML = "Reprovado"
+    }
+    else if (nota < 7){
+        resultado.innerHTML = "Recuperação"
+    }
+    else{
+        resultado.innerHTML = "Aprovado"
+    }
+}
