4c103e73668ea55076e8c6704a50717c2e871a06
BRUNA LOPES SILVA MENDES <bruna.lsmendes@senacsp.edu.br>
2026-09-08 21:49:50 -0300

    condicionais

--- js/script.js
+++ js/script.js
@@ -14,3 +14,39 @@ function verificar(){
         resultado.innerHTML = "Aprovado"
     }
 }
+
+let numero01, numero02, resultado02;
+
+function diferenca(){
+    numero01 = Number(document.getElementById("numero01").value);
+    numero02 = Number(document.getElementById("numero02").value);
+
+    resultado02 = document.getElementById("resultado02");
+
+    if (numero01>numero02){
+        resultado02.innerHTML = "Resultado= "+ (numero01 - numero02)
+    }
+    else{
+        resultado02.innerHTML = "Resultado= "+ (numero02 - numero01)
+    }
+}
+
+let n1, n2, n3, n4, md;
+
+function media(){
+   n1 = Number(document.getElementById("n1").value);
+   n2 = Number(document.getElementById("n2").value); 
+   n3 = Number(document.getElementById("n3").value); 
+   n4 = Number(document.getElementById("n4").value); 
+   
+   let conta = (n1+n2+n3+n4)/4;
+
+   md = document.getElementById("md");
+
+   if(conta >= 5){
+        md.innerHTML = "Aprovado! Sua média é: " + conta;
+   }
+   else{
+        md.innerHTML = "Reprovado! Sua média é: " + conta;
+   }
+}
